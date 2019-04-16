5f2b78104971c35033d744448de85d2f0a39866c
Wenliang Liu <wenliangliu@wenliangliu-macbookpro.roam.corp.google.com>
2019-04-16 11:40:44 -0700

    wrapped functions into class GPSParser

--- gps_raspberry_pi.cpp
+++ gps_raspberry_pi.cpp
@@ -1,16 +1,18 @@
+#include "util.hpp"
+
 #include <iostream>
 #include <thread>
 #include <chrono>
 #include <wiringPi.h>
 #include <wiringSerial.h>
-#include <string.h>
+#include <string>
 #include <time.h>
 #include <chrono>
 #include <vector>
-#include "util.hpp"
 #include <ctime>
 #include <time.h>
 #include <mutex>
+
 
 using namespace std;
 
@@ -19,10 +21,13 @@ std::mutex g_mutex;
 
 vector<string> g_gps_sentence_pool;
 
-// http://aprs.gids.nl/nmea
-void ParseGPS(const string& gps){
-    auto words = StrSplit(gps, ",");
-    if( StartWith(gps, "$GPVTG") ){
+
+class GPSParser{
+  public:
+    // http://aprs.gids.nl/nmea
+    void Parse(const string& gps){
+      auto words = StrSplit(gps, ",");
+      if( StartWith(gps, "$GPVTG") ){
         /* Track Made Good and Ground Spped.
            eg1. $GPVTG,360.0,T,348.7,M,000.0,N,000.0,K*43
            eg2. $GPVTG,054.7,T,034.4,M,005.5,N,010.2,K
@@ -52,7 +57,7 @@ void ParseGPS(const string& gps){
            x.x,K = Speed, Km/hr
            */
 
-    } else if( StartWith(gps, "$GPRMC") ){
+      } else if( StartWith(gps, "$GPRMC") ){
         /*
          * Recommended minimum specific GPS/Transit data
 
@@ -104,7 +109,7 @@ void ParseGPS(const string& gps){
          12   = Checksum
          */
 
-    } else if( StartWith(gps, "$GPGSA") ){
+      } else if( StartWith(gps, "$GPGSA") ){
         /*
          * $GPGSA
          GPS DOP and active satellites
@@ -125,139 +130,243 @@ void ParseGPS(const string& gps){
          16   = HDOP
          17   = VDOP
          */
-
-    } else if( StartWith(gps, "$GPGGA") ){
-        cout<<"Parsing:"<<endl<<gps<<endl;
-        PrintContainer(words);
-        Print(INFO, "words.size=", words.size());
-        int i = 1;
-        // i = 1 Time
-        string gps_time = words[i++];
-        // i = 2,3  Latitude, longitude
-        string latitude = words[i++], NE = words[i++];
-        // i = 4,5
-        string longitude = words[i++], SW = words[i++];
-        // 0 = INVALID
-        // 1 = GPS fix
-        // 2 = DGPS fix
-        // i = 6
-        int fix_quality = ParseNumberi(words[i++]);
-        int number_of_satellites = ParseNumberi(words[i++]);
-        Print(DEBUG, "i==", i);
-        // Horizontal Dilution of Precision (HDOP)
-        float hdop = ParseNumberf(words[i++]);
-        float altitude = ParseNumberf(words[i++]);
-        Print(DEBUG, "i==", i);
-        // i = 10. Unit
-        string altitute_unit = words[i++];
-        Print(DEBUG, "i==", i);
-        // Height of geoid above WGS85 ellipsoid
-        float height = ParseNumberf(words[i++]);
-        string height_uniq = words[i++];
-        Print(DEBUG, "i==", i);
-        // Time since last DGPS update
-        // DGPS reference station id and DGPS ref.statio.id  format x.x
-        string dgps_station = words[i++];
-        Print(DEBUG, "i==", i);
-        // i = 14
-        string check_sum = words[i++];
-        Print(DEBUG, "i==", i);
-
-        // time
-        struct tm parsed_tm;
-        gps_time.insert(2,":");
-        gps_time.insert(5,":");
-        strptime(gps_time.c_str(), "%H:%M:%S", &parsed_tm);
-
-        auto ca_time = parsed_tm;
-        ca_time.tm_hour = (ca_time.tm_hour - 7)%24;
-        Print(INFO, "UTC time is ", AscGpsTime(&parsed_tm));
-        Print(INFO, "CA time is ", AscGpsTime(&ca_time));
-
-        // long and lat
-        if( longitude.size() && latitude.size()){
-            string google_map_url = "www.google.com/maps/place/";
-            auto dot_pos = latitude.find('.');
-            latitude.insert(dot_pos-2,".");
-            latitude.erase(dot_pos+1,1);
-
-            dot_pos = longitude.find('.');
-            longitude.insert(dot_pos-2,".");
-            longitude.erase(dot_pos+1,1);
-
-            google_map_url += latitude + NE + "+" + longitude + SW;
-            Print(INFO, google_map_url);
-        }
-    } else{
+      } else if( StartWith(gps, "$GPGGA") ){
+        Print(INFO, "Parsing:", gps);
+        ParseGPGGA(words);
+      } else{
         Print(ERROR, "Not a PGS sentence. Skip.");
+      }
     }
-}
+  private:
+    void ParseGPGGA(const vector<string>& words){
+      /*
+         $GPGGA
+         Global Positioning System Fix Data
 
+         Name	Example Data	Description
+         Sentence Identifier	$GPGGA	Global Positioning System Fix Data
+         Time	170834	17:08:34 Z
+         Latitude	4124.8963, N	41d 24.8963' N or 41d 24' 54" N
+         Longitude	08151.6838, W	81d 51.6838' W or 81d 51' 41" W
+         Fix Quality:
+         - 0 = Invalid
+         - 1 = GPS fix
+         - 2 = DGPS fix	1	Data is from a GPS fix
+         Number of Satellites	05	5 Satellites are in view
+         Horizontal Dilution of Precision (HDOP)	1.5	Relative accuracy of horizontal position
+         Altitude	280.2, M	280.2 meters above mean sea level
+         Height of geoid above WGS84 ellipsoid	-34.0, M	-34.0 meters
+         Time since last DGPS update	blank	No last update
+         DGPS reference station id	blank	No station id
+         Checksum	*75	Used by program to check for transmission errors
+         Courtesy of Brian McClure, N8PQI.
 
-void ParseGPS_Thread(){
-    while(true){
-        g_mutex.lock();
-        auto gps_sentence = g_gps_sentence_pool;
-        g_gps_sentence_pool.clear();
-        while( gps_sentence.size() ){
-            string gps = gps_sentence.back();
-            gps_sentence.pop_back();
-            try{
-                ParseGPS(gps);
-            }catch(exception e){
-                Print(ERROR, "ParseGPS_Thread");
-                cout<<e.what()<<endl;
-                exit (-1);
-            }
+         Global Positioning System Fix Data. Time, position and fix related data for a GPS receiver.
+
+         eg2. $--GGA,hhmmss.ss,llll.ll,a,yyyyy.yy,a,x,xx,x.x,x.x,M,x.x,M,x.x,xxxx
+
+         hhmmss.ss = UTC of position
+         llll.ll = latitude of position
+         a = N or S
+         yyyyy.yy = Longitude of position
+         a = E or W
+         x = GPS Quality indicator (0=no fix, 1=GPS fix, 2=Dif. GPS fix)
+         xx = number of satellites in use
+         x.x = horizontal dilution of precision
+         x.x = Antenna altitude above mean-sea-level
+         M = units of antenna altitude, meters
+         x.x = Geoidal separation
+         M = units of geoidal separation, meters
+         x.x = Age of Differential GPS data (seconds)
+         xxxx = Differential reference station ID
+         eg3. $GPGGA,hhmmss.ss,llll.ll,a,yyyyy.yy,a,x,xx,x.x,x.x,M,x.x,M,x.x,xxxx*hh
+         1    = UTC of Position
+         2    = Latitude
+         3    = N or S
+         4    = Longitude
+         5    = E or W
+         6    = GPS quality indicator (0=invalid; 1=GPS fix; 2=Diff. GPS fix)
+         7    = Number of satellites in use [not those in view]
+         8    = Horizontal dilution of position
+         9    = Antenna altitude above/below mean sea level (geoid)
+         10   = Meters  (Antenna height unit)
+         11   = Geoidal separation (Diff. between WGS-84 earth ellipsoid and
+         mean sea level.  -=geoid is below WGS-84 ellipsoid)
+         12   = Meters  (Units of geoidal separation)
+         13   = Age in seconds since last update from diff. reference station
+         14   = Diff. reference station ID#
+         15   = Checksum
+         */
+      PrintContainer(DEBUG, words);
+      Print(DEBUG, "words.size=", words.size());
+      int i = 1;
+      // i = 1 Time
+      string gps_time = words[i++];
+      // i = 2,3  Latitude, longitude
+      string latitude = words[i++], NE = words[i++];
+      // i = 4,5
+      string longitude = words[i++], SW = words[i++];
+      // 0 = INVALID
+      // 1 = GPS fix
+      // 2 = DGPS fix
+      // i = 6
+      string str_fix_quality = words[i++];
+      string str_umber_of_satellites = words[i++];
+      Print(DEBUG, "i==", i);
+      // Horizontal Dilution of Precision (HDOP)
+      string str_hdop = words[i++];
+      string str_altitude = words[i++];
+      Print(DEBUG, "i==", i);
+      // i = 10. Unit
+      string altitute_unit = words[i++];
+      Print(DEBUG, "i==", i);
+      // Height of geoid above WGS85 ellipsoid
+      string str_height = words[i++];
+      string height_unit = words[i++];
+      Print(DEBUG, "i==", i);
+      // Time since last DGPS update
+      // DGPS reference station id and DGPS ref.statio.id  format x.x
+      string dgps_station = words[i++];
+      Print(DEBUG, "i==", i);
+      // i = 14
+      string check_sum = words[i++];
+      Print(DEBUG, "i==", i);
+
+      // time
+      ParseGpsTime(gps_time);
+    
+      // long and lat
+      ParseLatAndLong(latitude, longitude, NE, SW);
+
+      // fix_quailty
+      if( str_fix_quality.size() ){
+        int fix_quality = stoi(str_fix_quality);
+        switch(fix_quality){
+          case 1: Print(INFO, "GPS fix");
+          case 2: Print(INFO, "DGPS fix");
+          default:Print(INFO, "Invalid fix quality");
         }
-        g_mutex.unlock();
-        std::this_thread::sleep_for(chrono::seconds(1));
+      }
+
+      // number of satellites
+      if( str_umber_of_satellites.size()){
+        int number_of_satellites = stoi(str_umber_of_satellites);
+        Print(INFO, "Satellites in use:", number_of_satellites);
+      }
+
+      // HDOP
+      // Skip
+
+      // Altitude
+      if( !str_altitude.empty()){
+        float altitude = stof(str_altitude);
+        Print(INFO, "Altitude: ", altitude," ", altitute_unit);
+      }
+
+      // Height
+      if(!str_height.empty()){
+        float height = stof(str_height);
+        Print(INFO, "Height: ", height," ", height_unit);
+      }
     }
+
+    void ParseGpsTime(const string& time){
+      string gps_time = time;
+      struct tm parsed_tm;
+      gps_time.insert(2,":");
+      gps_time.insert(5,":");
+      strptime(gps_time.c_str(), "%H:%M:%S", &parsed_tm);
+
+      auto ca_time = parsed_tm;
+      ca_time.tm_hour = (ca_time.tm_hour - 7)%24;
+      Print(INFO, "UTC time is ", AscGpsTime(&parsed_tm));
+      Print(INFO, "CA time is ", AscGpsTime(&ca_time));
+    }
+
+    void ParseLatAndLong(const string& lat, const string& longi,
+                        const string& NE, const string& SW){
+      string latitude = lat, longitude = longi;
+      if( longitude.size() && latitude.size()){
+        string google_map_url = "www.google.com/maps/place/";
+        auto dot_pos = latitude.find('.');
+        latitude.insert(dot_pos-2,".");
+        latitude.erase(dot_pos+1,1);
+
+        dot_pos = longitude.find('.');
+        longitude.insert(dot_pos-2,".");
+        longitude.erase(dot_pos+1,1);
+
+        google_map_url += latitude + NE + "+" + longitude + SW;
+        Print(INFO, google_map_url);
+      }
+    }
+};
+
+void ParseGPS_Thread( GPSParser* gps){
+  while(true){
+    g_mutex.lock();
+    auto gps_sentence_pool = g_gps_sentence_pool;
+    g_gps_sentence_pool.clear();
+    g_mutex.unlock();
+
+    while( gps_sentence_pool.size() ){
+      string gps_sentence = gps_sentence_pool.back();
+      gps_sentence_pool.pop_back();
+      try{
+        gps->Parse(gps_sentence);
+      }catch(exception e){
+        Print(ERROR, "ParseGPS_Thread");
+        cout<<e.what()<<endl;
+        exit (-1);
+      }
+    }
+    std::this_thread::sleep_for(chrono::seconds(1));
+  }
+
 }
 
 void ReceiveGPS_Thread( int fd ){
-    string buffer;
-    while(true){
-        try{
-            if(::serialDataAvail(fd)){
-                int ch = ::serialGetchar(fd);
-                if( ch >= 0 && ch <= 128 ){
-                    if( (char)ch == '$' && buffer.size() ){
-                        g_mutex.lock();
-                        g_gps_sentence_pool.push_back(std::move(buffer));
-                        g_mutex.unlock();
-                    }
-                    buffer.push_back(ch);
-                }
-            }
-        }catch( exception e){
-            Print(ERROR, "ReceiveGPS_Thread");
-            cout<<e.what()<<endl;
+  string buffer;
+  while(true){
+    try{
+      if(::serialDataAvail(fd)){
+        int ch = ::serialGetchar(fd);
+        if( ch >= 0 && ch <= 128 ){
+          if( (char)ch == '$' && buffer.size() ){
+            g_mutex.lock();
+            g_gps_sentence_pool.push_back(std::move(buffer));
+            g_mutex.unlock();
+          }
+          buffer.push_back(ch);
         }
+      }
+    }catch( exception e){
+      Print(ERROR, "ReceiveGPS_Thread");
+      cout<<e.what()<<endl;
     }
+  }
 }
 
 
 int main(int argc, char**argv){
-    wiringPiSetup();
+  wiringPiSetup();
 
-    int fd = serialOpen("/dev/ttyUSB0", 9600);
-    if( fd < 0 ) { cout<<"Cannot open serial"<<endl; return 1; }
+  int fd = serialOpen("/dev/ttyUSB0", 9600);
+  if( fd < 0 ) { cout<<"Cannot open serial"<<endl; return 1; }
 
-    try{
-        thread receive_gps( ReceiveGPS_Thread, fd );
-        thread parse_gps( ParseGPS_Thread );
-        receive_gps.join();
-        parse_gps.join();
-    }catch(exception e){
-        Print(ERROR, "Main exception");
-        cout<<e.what()<<endl;
-    }
+  GPSParser gps;
+  try{
+    thread receive_gps( ReceiveGPS_Thread, fd );
+    thread parse_gps( ParseGPS_Thread, &gps );
+    receive_gps.join();
+    parse_gps.join();
+  }catch(exception e){
+    Print(ERROR, "Main exception");
+    cout<<e.what()<<endl;
+  }
 
-    cout<<"Exiting..."<<endl;
-    cout<<"Exiting..."<<endl;
-    cout<<"Exiting..."<<endl;
-    serialClose(fd);
+  cout<<"Exiting..."<<endl;
+  serialClose(fd);
 
-    return 0;
+  return 0;
 }
--- util.hpp
+++ util.hpp
@@ -15,67 +15,70 @@ using namespace std;
 #define DISABLE_DEBUG_MSG
 
 bool StartWith(const string& str, const string& word){
-    if(str.size() < word.size()) return false;
-    bool same = true;
-    unsigned int i = 0;
-    for( ;i<word.size() && same; i++)
-        same = word[i] == str[i];
-    return i == word.size();
+  if(str.size() < word.size()) return false;
+  bool same = true;
+  unsigned int i = 0;
+  for( ;i<word.size() && same; i++)
+    same = word[i] == str[i];
+  return i == word.size();
 }
 
 
 vector<string> StrSplit( const string& str, const string& delim ){
-    vector<string> ret;
-    size_t last = 0, next = 0;
+  vector<string> ret;
+  size_t last = 0, next = 0;
 
-    while( (next = str.find(delim,last)) != string::npos ){
-        if(next-last == 0){
-            ret.push_back("");
-        }else{
-            ret.push_back( str.substr(last, next-last) );
-        }
-        next++;
-        last = next;
+  while( (next = str.find(delim,last)) != string::npos ){
+    if(next-last == 0){
+      ret.push_back("");
+    }else{
+      ret.push_back( str.substr(last, next-last) );
     }
-    ret.push_back(str.substr(last));
-    return ret;
+    next++;
+    last = next;
+  }
+  ret.push_back(str.substr(last));
+  return ret;
 }
 
 
 template <typename T>
-void PrintContainer(const T& container){
-    for(auto it = container.begin(); it!=container.end(); it++){
-        cout<<*it<<" ";
-    }
-    cout<<endl;
+void PrintContainer(Level l, const T& container){
+#ifdef DISABLE_DEBUG_MSG
+  if(l == DEBUG) return;
+#endif
+  for(auto it = container.begin(); it!=container.end(); it++){
+    cout<<*it<<" ";
+  }
+  cout<<endl;
 }
 
 template <typename ...T>
 void Print(Level l,  T... args ){
 #ifdef DISABLE_DEBUG_MSG
-    if(l == DEBUG) return;
+  if(l == DEBUG) return;
 #endif
-    (cout<<...<<args);
-    cout<<endl;
+  (cout<<...<<args);
+  cout<<endl;
 }
 
 int ParseNumberi(const string& str){
-    return std::stoi(str);
+  return std::stoi(str);
 }
 
 float ParseNumberf(const string& str){
-    if(str == "") return 0.0f;
-    return std::stof(str);
+  if(str == "") return 0.0f;
+  return std::stof(str);
 }
 
 string AscGpsTime( std::tm* tm ){
-    char ch[32];
-    std::sprintf(ch, "%d:%d:%d\n", tm->tm_hour, tm->tm_min, tm->tm_sec);
-    return string(ch);
+  char ch[32];
+  std::sprintf(ch, "%d:%d:%d\n", tm->tm_hour, tm->tm_min, tm->tm_sec);
+  return string(ch);
 }
 
 #define  RUN_TEST(FuncName) {\
-printf("Testing %s\n", #FuncName);\
+  printf("Testing %s\n", #FuncName);\
   FuncName();\
   printf("%s passed!\n\n", #FuncName);\
 }
@@ -83,35 +86,35 @@ printf("Testing %s\n", #FuncName);\
 
 
 TEST(TEST_STRSPLIT){
-    string a = "a,b,c";
-    auto b = StrSplit(a, ",");
-    vector<string> eq_v = {"a","b","c"};
-    assert(b == eq_v);
+  string a = "a,b,c";
+  auto b = StrSplit(a, ",");
+  vector<string> eq_v = {"a","b","c"};
+  assert(b == eq_v);
 }
 
 TEST(TEST_STRSPLIT2){
-    string a = "a123";
-    auto b = StrSplit(a, ".");
-    assert( b == vector<string>{"a123"} );
+  string a = "a123";
+  auto b = StrSplit(a, ".");
+  assert( b == vector<string>{"a123"} );
 }
 
 
 TEST(TEST_STRSPLIT3){
-    string a = "";
-    auto b = StrSplit(a," ");
-    assert( b == vector<string>{""});
+  string a = "";
+  auto b = StrSplit(a," ");
+  assert( b == vector<string>{""});
 }
 
 TEST(TEST_STRSPLIT4){
-    string a = ",,,";
-    auto b = StrSplit(a,",");
-    auto cmp = vector<string>{"","","",""};
-    assert( b == cmp );
+  string a = ",,,";
+  auto b = StrSplit(a,",");
+  auto cmp = vector<string>{"","","",""};
+  assert( b == cmp );
 }
 
 void TestUtil(){
-    RUN_TEST(TEST_STRSPLIT);
-    RUN_TEST(TEST_STRSPLIT2);
-    RUN_TEST(TEST_STRSPLIT3);
-    RUN_TEST(TEST_STRSPLIT4);
+  RUN_TEST(TEST_STRSPLIT);
+  RUN_TEST(TEST_STRSPLIT2);
+  RUN_TEST(TEST_STRSPLIT3);
+  RUN_TEST(TEST_STRSPLIT4);
 }
