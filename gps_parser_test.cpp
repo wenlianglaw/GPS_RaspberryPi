6622b41f44d51e3c17513dfab57ee954a864441b
wenlianglaw <wenlianglaw@gmail.com>
2019-04-19 21:29:39 -0700

    fixed some bugs

--- gps_parser_test.cpp
+++ gps_parser_test.cpp
@@ -1,19 +1,27 @@
+#undef DISABLE_DEBUG_MSGo
+
 #include "gps_parser.h"
+
 #include "testcommon.h"
 #include <iostream>
 
-const char k_gpgga_test[] = "";
-const char k_gpgsa_test[] = "";
+using namespace std;
 
-TEST(GPSPARSER_GPGGA){
+const char k_gpgga_test[] = "";
+const char k_gpgsa_test[] = "$GPGSA,A,1,,,,,,,,,,,,,,,*1E";
+
+TEST(GPSPARSER_WORKFLOW_GPGGA){
+  GPSUnit gps_unit;
+  gps_parser::Parse(k_gpgsa_test, &gps_unit);
+  // assert no fail
 }
 
-TEST(GPSPARSER_GPGSA){
+TEST(GPSPARSER_WORKFLOW_GPGSA){
 }
 
 void RunTests(){
-  RUN_TEST(GPSPARSER_GPGGA);
-  RUN_TEST(GPSPARSER_GPGSA);
+  RUN_TEST(GPSPARSER_WORKFLOW_GPGGA);
+  RUN_TEST(GPSPARSER_WORKFLOW_GPGSA);
 
   std::cout<<"All GPS Parser tests passed!"<<std::endl;
 }
