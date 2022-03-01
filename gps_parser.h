a1cdef6b589b5b92ed9bee65cd64e3f2fd54e33b
wenlianglaw <wenlianglaw@gmail.com>
2022-03-01 02:56:31 -0800

    Refractored the gps_parser.    Used pimpl.    Parse functions now return the parse status (bool).    Now we have class parser instead of the static functions.

--- gps_parser.h
+++ gps_parser.h
@@ -1,22 +1,35 @@
 #ifndef GPS_RASPBERRYPI_GPS_PARSER_H_
 #define GPS_RASPBERRYPI_GPS_PARSER_H_
 
+#include "gps_unit.h"
+
 #include <vector>
 #include <string>
+#include <memory>
 
-#include "gps_unit.h"
 
 using std::string;
 
 namespace gps_parser{
-  /* IMPORTANT     IMPORTANT
-   * This fix comes with the GPS module. */
-  static float fix = 1.6667;
+  class GPSParser {
+    // This class hides a lot of private funcs and variables.
+    class Impl;
+    std::unique_ptr<Impl> pimpl_;
 
-  /* http://aprs.gids.nl/nmea
-   * Parse a gps sentence into gps_unit.
-   */
-  void Parse(const std::string& gps_msg, GPSUnit* gps_unit);
+    public:
+      GPSParser();
+      ~GPSParser();
+
+      /* http://aprs.gids.nl/nmea
+       * Parse a gps sentence into gps_unit.
+       *
+       * Returns:
+       *   true if this message is parsed.
+       *   false if any error occurs.
+       */
+      bool Parse(const std::string& gps_msg, GPSUnit* gps_unit);
+  };
+
 } // namespace gps_parser
 
 
