f2a56190a931bf864c1550641758ab992000c461
wenlianglaw <wenlianglaw@gmail.com>
2022-03-06 23:44:36 -0800

    Removed the write cusomt GPS message function.

--- file_writer.h
+++ file_writer.h
@@ -1,5 +1,5 @@
 /* The file_rw stands for file writer.
- * It provides the funtion to write the GPS message or the gps_unit to a local file.
+ * It provides the funtion to write the GPS message.
  */
 
 #ifndef FILE_WR_H_
@@ -9,10 +9,11 @@
 #include <string>
 #include <string_view>
 #include <fstream>
+#include <string>
 
-#include "gps_unit.h"
 
 namespace gps_parser{
+
 class FileWriter {
   public:
     FileWriter();
@@ -30,24 +31,15 @@ class FileWriter {
     // Writes the raw message to the raw log file.
     void WriteRawMessage(std::string_view msg);
 
-    // Writes the field that we are interested to the parsed log file.
-    void WriteGpsUnit(GPSUnit unit);
-
   private:
     std::string output_dir_;
 
     // The raw messages go here.
     std::string raw_log_file_name_;
     std::ofstream raw_log_file_;
-
-    // The gps unit messages go here.
-    std::string gps_unit_log_file_name_;
-    std::ofstream gps_unit_log_file_;
-
   private:
     std::string GetDefaultOutputDir();
     void UpdateRawLogFileName();
-    void UpdateGpsUnitLogFileName();
 
     // Creates output dir if it doesn't exist.
     void CreateOutPutDir();
