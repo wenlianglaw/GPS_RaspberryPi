8e4ad9e841ea01e90e5e9cb8215d74145cf19225
wenlianglaw <wenlianglaw@gmail.com>
2022-03-06 23:33:56 -0800

    log_analyzer dir created and Makefile chagnes.

---
+++ log_analyzer/log_analyzer.cpp
@@ -0,0 +1,32 @@
+#include "log_analyzer.h"
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+namespace gps_parser{
+
+
+void LogAnalyzer::Analyze(std::string_view dir_or_file) {
+
+  if (fs::is_directory(dir_or_file)) {
+    AnalyzeADir(dir_or_file);
+  }
+
+  if (fs::is_regular_file(dir_or_file)) {
+    AnalyzeAFile(dir_or_file);
+  }
+}
+
+void LogAnalyzer::AnalyzeADir(std::string_view dir_name) {
+  for (auto&& dir_entry : fs::directory_iterator{dir_name}) {
+    AnalyzeAFile(dir_entry.path().string());
+  }
+}
+
+void LogAnalyzer::AnalyzeAFile(std::string_view filename) {
+  if (!fs::is_regular_file(filename)) {
+    return;
+  }
+}
+
+}  // namespace gps_parser
---
+++ log_analyzer/log_analyzer.h
@@ -0,0 +1,43 @@
+#include <string>
+#include <string_view>
+#include "../gps_parser.h"
+
+namespace gps_parser{
+
+// This is the structure that will be written into the analyze
+// files.
+struct CustomGPSMsgToWrite{
+  float latitude_;
+  std::string ew_;
+  float longitude_;
+  std::string ns_;
+  // "%Y-%m-%d %H:%M:%S"
+  std::string date_time_;
+};
+
+class LogAnalyzer {
+  public:
+    std::string file_prefix_ = "parsed_";
+
+  private:
+    std::string dir_or_file_;
+
+  public:
+    LogAnalyzer() = default;
+    ~LogAnalyzer() = default;
+    
+  public:
+    // If dir_or_file is directory.  Analyze all the logs in
+    // the direcotry.
+    // If it is a file, only analyze this file.
+    void Analyze(std::string_view dir_or_file);
+
+  private:
+    // Analyzes a file.  If filename is not a file, then do nothing.
+    void AnalyzeAFile(std::string_view filename);
+
+    // Analyzes all the logs in the directory.
+    void AnalyzeADir(std::string_view dir_name);
+};
+
+}  // namespace gps_parser
